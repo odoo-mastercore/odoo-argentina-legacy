ff14ded201d109286235ad47a2b5697354c95ef6
FAR <far@mastercore.net>
2020-06-03 14:36:49 -0300

    wip: compare w ctmil/odoo-argentina

---
+++ l10n_ar_account_withholding/models/__init__.py
@@ -0,0 +1,10 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from . import res_company
+from . import account_tax
+from . import account_move
+from . import afip
+from . import account_payment_group
+from . import res_partner
