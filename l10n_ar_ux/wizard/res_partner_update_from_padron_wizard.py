b3162c945e8401a9f19c3409cf02c6626eea4ac1
FAR <far@mastercore.net>
2020-06-06 17:15:46 -0300

    feat: Request of padron A5 from AFIP

--- l10n_ar_ux/wizard/res_partner_update_from_padron_wizard.py
+++ l10n_ar_ux/wizard/res_partner_update_from_padron_wizard.py
@@ -80,7 +80,7 @@ class ResPartnerUpdateFromPadronWizard(models.TransientModel):
             'empleador_padron',
             'integrante_soc_padron',
             'last_update_padron',
-            'afip_responsability_type_id',
+            'l10n_ar_afip_responsability_type_id',
             # 'constancia',
         ]
         return [
