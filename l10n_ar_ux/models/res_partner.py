56c651b2d2ae41c4610a77b3186a67de7d31c0ff
FAR <far@mastercore.net>
2020-06-05 22:05:54 -0300

    feat: Request of padron A5 from AFIP

--- l10n_ar_ux/models/res_partner.py
+++ l10n_ar_ux/models/res_partner.py
@@ -125,3 +125,97 @@ class ResPartner(models.Model):
             ).create({})
             wiz.change_partner()
             wiz.update_selection()
+
+    def get_data_from_padron_afip(self):
+        self.ensure_one()
+        cuit = self.vat
+        # GET COMPANY
+        # if there is certificate for user company, use that one, if not
+        # use the company for the first certificate found
+        company = self.env.user.company_id
+        env_type = company._get_environment_type()
+        try:
+            certificate = company.get_key_and_certificate(
+                company._get_environment_type())
+        except Exception:
+            certificate = self.env['afipws.certificate'].search([
+                ('alias_id.type', '=', env_type),
+                ('state', '=', 'confirmed'),
+            ], limit=1)
+            if not certificate:
+                raise UserError(_(
+                    'Not confirmed certificate found on database'))
+            company = certificate.alias_id.company_id
+        _logger.debug("____Calling ws_sr_padron_a5_____")
+        padron = company.get_connection('ws_sr_padron_a5').connect()
+        _logger.debug("____Called ws_sr_padron_a5_____")
+        padron.Consultar(cuit)
+        # porque imp_iva activo puede ser S o AC
+        imp_iva = padron.imp_iva
+        if imp_iva == 'S':
+            imp_iva = 'AC'
+        elif imp_iva == 'N':
+            # por ej. monotributista devuelve N
+            imp_iva = 'NI'
+        vals = {
+            'name': padron.denominacion,
+            # 'name': padron.tipo_persona,
+            # 'name': padron.tipo_doc,
+            # 'name': padron.dni,
+            'estado_padron': padron.estado,
+            'street': padron.direccion,
+            'city': padron.localidad,
+            'zip': padron.cod_postal,
+            'actividades_padron': self.actividades_padron.search(
+                [('code', 'in', padron.actividades)]).ids,
+            'impuestos_padron': self.impuestos_padron.search(
+                [('code', 'in', padron.impuestos)]).ids,
+            'imp_iva_padron': imp_iva,
+            # TODAVIA no esta funcionando
+            # 'imp_ganancias_padron': padron.imp_ganancias,
+            'monotributo_padron': padron.monotributo,
+            'actividad_monotributo_padron': padron.actividad_monotributo,
+            'empleador_padron': padron.empleador == 'S' and True,
+            'integrante_soc_padron': padron.integrante_soc,
+            'last_update_padron': fields.Date.today(),
+        }
+        ganancias_inscripto = [10, 11]
+        ganancias_exento = [12]
+        if set(ganancias_inscripto) & set(padron.impuestos):
+            vals['imp_ganancias_padron'] = 'AC'
+        elif set(ganancias_exento) & set(padron.impuestos):
+            vals['imp_ganancias_padron'] = 'EX'
+        elif padron.monotributo == 'S':
+            vals['imp_ganancias_padron'] = 'NC'
+        else:
+            _logger.debug(
+                "We couldn't get impuesto a las ganancias from padron, you"
+                "must set it manually")
+        if padron.provincia:
+            # if not localidad then it should be CABA.
+            if not padron.localidad:
+                state = self.env['res.country.state'].search([
+                    ('code', '=', 'ABA'),
+                    ('country_id.code', '=', 'AR')], limit=1)
+            # If localidad cant be caba
+            else:
+                state = self.env['res.country.state'].search([
+                    ('name', 'ilike', padron.provincia),
+                    ('code', '!=', 'ABA'),
+                    ('country_id.code', '=', 'AR')], limit=1)
+            if state:
+                vals['state_id'] = state.id
+        if imp_iva == 'NI' and padron.monotributo == 'S':
+            vals['afip_responsability_type_id'] = self.env.ref(
+                'l10n_ar_account.res_RM').id
+        elif imp_iva == 'AC':
+            vals['afip_responsability_type_id'] = self.env.ref(
+                'l10n_ar_account.res_IVARI').id
+        elif imp_iva == 'EX':
+            vals['afip_responsability_type_id'] = self.env.ref(
+                'l10n_ar_account.res_IVAE').id
+        else:
+            _logger.debug(
+                "We couldn't infer the AFIP responsability from padron, you"
+                "must set it manually.")
+        return vals
