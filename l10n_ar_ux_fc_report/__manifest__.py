0c0b2eff333492afc7b0d0f4ca9b3997544d40e5
odoo-mastercore <github@mastercore.net>
2021-04-03 19:33:36 -0300

    [ADD] Foering currency invoice report

---
+++ l10n_ar_ux_fc_report/__manifest__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Argentinian Accounting UX',
+    'version': "13.0.1.12.0",
+    'category': 'Localization/Argentina',
+    'sequence': 15,
+    'author': 'ADHOC SA, MASTERCORE SAS',
+    'website': 'https://sinapsys.global',
+    'license': 'AGPL-3',
+    'summary': '',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/report_invoice.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
