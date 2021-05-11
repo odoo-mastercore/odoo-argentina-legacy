6ccf6ac932f93531bb86545edfad9fd4328a41a1
far-mastercore <far@sinapsys.global>
2021-05-10 23:36:50 -0300

    [REF] remove bytes casting on wsaa.SignTRA

--- l10n_ar_afipws/models/res_company.py
+++ l10n_ar_afipws/models/res_company.py
@@ -203,7 +203,7 @@ class ResCompany(models.Model):
                 # ticket (TRA)
                 tra = wsaa.CreateTRA(service=service, ttl=DEFAULT_TTL)
                 # cryptographically sing the access ticket
-                cms = wsaa.SignTRA(bytes(tra, encoding='utf-8'),
+                cms = wsaa.SignTRA(tra,
                     certificate, private_key)
                 # connect to the webservice:
                 wsaa.Conectar(cache, wsdl, proxy)
